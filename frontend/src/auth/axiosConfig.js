2f943e6fca47b91be53e35d1d7d8f33390309890
Mubashir Hussain <mubashir.hussain@arbisoft.com>
2021-10-22 19:53:08 +0500

    Emptied Master

--- frontend/src/auth/axiosConfig.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-axios.defaults.xsrfHeaderName = "X-CSRFToken"
-axios.defaults.xsrfCookieName = 'csrftoken'
-axios.defaults.withCredentials = true
-axios.defaults.baseURL = "../";
-export default axios
--- frontend/src/books/index.jsx
+++
@@ -1,9 +0,0 @@
-
-export { BookDetails } from './bookDetails';
-export { EditBook } from './editBook';
-export { NewBook } from './newBook';
-export { BooksList } from './bookList';
-export { RequestsList } from './requestList';
-export { RecordsList } from './recordList';
-export { UserDetails } from './userDetails';
-export { UsersList } from './userList';
